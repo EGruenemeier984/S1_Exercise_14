a924c714d92370e668d3c9098f1dad2a13dfb51e
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-03-29 14:36:19 -0700

    Setup linked nested list
    Created a node list that is nested as well as its linked to different parts of the page.

--- bc_outline.js
+++ bc_outline.js
@@ -51,14 +51,58 @@ function createList(source, outlineList) {
 	// headings for the outline
 	var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
 
+	// Previous level of headings
+	var prevLevel = 0;
+
+	// Running total of the article headings
+	var headNum = 0;
+
 	// loop through all of the child nodes of source articles until no child nodes are left.
 	for (var n = source.firstChild; n !== null; n = n.nextSibling) {
 		var headLevel = headings.indexOf(n.nodeName);
 
 		if (headLevel !== -1) {
-			var listItem = document.createElement("li");
-			listItem.innerHTML = n.firstChild.nodeValue;
-			outlineList.appendChild(listElem);
+			// Add an id to the heading if it is missing.
+			headNum++;
+			if (n.hasAttribute("id") === false) {
+				n.setAttribute("id", "head" + headNum);
+			}
+			var listElem = document.createElement("li");
+
+			// Create hypertext links to the document headings 
+			var linkElem = document.createElement("a");
+			linkElem.innerHTML = n.innerHTML;
+			linkElem.setAttribute("href", "#" + n.id);
+
+			// Append the Hypertext link to the list item.
+			listElem.appendChild(linkElem);
+
+			if (headLevel === prevLevel) {
+				// Append the list item to the current list
+				outlineList.appendChild(listElem);
+			} else if (headLevel > prevLevel) {
+				// start a new nested list 
+				var nestedList = document.createElement("ol");
+				nestedList.appendChild(listElem);
+				// Append nested list to the last item in the current list
+				outlineList.lastChild.appendChild(nestedList);
+				// change the current list to the nested list.
+				outlineList = nestedList;
+			} else {
+				// Append the list item to a higher list.
+				// Calculate the difference between the current and previous level.
+				var levelUp = prevLevel - headLevel;
+				// Go up to the higher level 
+				for (var i = 1; i <= levelUp; i++) {
+					outlineList = outlineList.parentNode.parentNode;
+				}
+				outlineList.appendChild(listElem);
+			}
+
+			// Update the value of prevLevel.
+			prevLevel = headLevel;
+
+
 		}
 	}
 }
--- bc_switch.js
+++ bc_switch.js
@@ -16,3 +16,29 @@
    view and page view
    
 */
+
+window.addEventListener("load", setupStyles);
+
+function setupStyles() {
+    // Create a link element for the page view styles.
+    var pageStyle = document.createElement("link");
+    pageStyle.setAttribute("href", "bc_page.css");
+    pageStyle.setAttribute("rel", "stylesheet");
+    pageStyle.setAttribute("disabled", "disabled");
+
+    // Append the link element to the document head.
+    document.head.appendChild(pageStyle);
+    pageStyle.disabled = true;
+
+    // Insert buttons for the style switcher 
+    var buttonDIV = document.createElement("div");
+    buttonDIV.setAttribute("id", "styleButtons");
+
+    var webButton = document.createElement("input");
+    webButton.setAttribute("type", "button");
+    webButton.setAttribute("value", "Web View");
+
+    var pageButton = document.createElement("input");
+    pageButton.setAttribute("type", "button");
+    pageButton.setAttribute("value", "Page View");
+}
